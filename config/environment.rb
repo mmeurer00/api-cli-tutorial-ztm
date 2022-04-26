9796356e0a721bbe6a62ed4651aad8a592618d46
Maxine <maxinemeurer@yahoo.com>
2022-04-25 23:46:46 -0400

    add gems pry, httparty, colorize, dotenv, configure environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+require 'httparty'
+require 'colorize'
+require 'colorized_string'
+require 'dotenv/load'
+
+require_relative '../lib/modules/action.rb'
+require_relative '../lib/api.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/object.rb'
